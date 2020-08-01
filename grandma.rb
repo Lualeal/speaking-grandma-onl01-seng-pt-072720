251da8bf87bcd30b3861c4197dcb4f30d8b1329e
Luana Leal <luana.leal.eq@gmail.com>
2020-08-01 02:11:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,20 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+
+
+#if speak_to_grandma  == "Hi Nana, how are you?"
+ # puts "HUH?! SPEAK UP, SONNY!"
+#elsif speak_to_grandma == "Hi!"
+ # puts "HUH?! SPEAK UP, SONNY!"
+#elsif  speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+ # puts "NO, NOT SINCE 1938!"
+ 
+ speak_to_grandma = "I LOVE YOU GRANDMA!"
+
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
+  
