b07aa98f751e20bfe17e7b4fe0a49316c21d1103
Luana Leal <luana.leal.eq@gmail.com>
2020-08-01 02:31:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -26,7 +26,12 @@
 
 
 def speak_to_grandma(phrase)
-  return "HUH?! SPEAK UP, SONNY!"
+  
+  if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 
